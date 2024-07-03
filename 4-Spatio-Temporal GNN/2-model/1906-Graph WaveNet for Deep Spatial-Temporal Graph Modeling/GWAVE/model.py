e081321973fa1e55afc7fcb32049a378171d1f42
fczhang0606 <fczhang0606@gmail.com>
2024-07-03 16:06:02 +0800

    20240703

--- 4-Spatio-Temporal GNN/2-model/1906-Graph WaveNet for Deep Spatial-Temporal Graph Modeling/GWAVE/model.py
+++ 4-Spatio-Temporal GNN/2-model/1906-Graph WaveNet for Deep Spatial-Temporal Graph Modeling/GWAVE/model.py
@@ -1,53 +1,65 @@
+import sys
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
-import sys
 
 
-class nconv(nn.Module):
-    def __init__(self):
-        super(nconv,self).__init__()
 
-    def forward(self,x, A):
-        x = torch.einsum('ncvl,vw->ncwl',(x,A))
+class nconv(nn.Module) :
+
+    def __init__(self) :
+        super(nconv, self).__init__()
+
+    def forward(self, x, A) :
+        x = torch.einsum('ncvl,vw->ncwl', (x, A))
         return x.contiguous()
 
-class linear(nn.Module):
-    def __init__(self,c_in,c_out):
-        super(linear,self).__init__()
+
+class linear(nn.Module) :
+
+    def __init__(self, c_in, c_out) :
+        super(linear, self).__init__()
         self.mlp = torch.nn.Conv2d(c_in, c_out, kernel_size=(1, 1), padding=(0,0), stride=(1,1), bias=True)
 
-    def forward(self,x):
+    def forward(self, x) :
         return self.mlp(x)
 
-class gcn(nn.Module):
-    def __init__(self,c_in,c_out,dropout,support_len=3,order=2):
-        super(gcn,self).__init__()
+
+class gcn(nn.Module) :
+
+
+    def __init__(self, c_in, c_out, dropout, support_len=3, order=2) :
+
+        super(gcn, self).__init__()
         self.nconv = nconv()
         c_in = (order*support_len+1)*c_in
-        self.mlp = linear(c_in,c_out)
+        self.mlp = linear(c_in, c_out)
         self.dropout = dropout
         self.order = order
 
-    def forward(self,x,support):
+
+    def forward(self, x, support) :
         out = [x]
-        for a in support:
-            x1 = self.nconv(x,a)
+        for a in support :
+            x1 = self.nconv(x, a)
             out.append(x1)
-            for k in range(2, self.order + 1):
-                x2 = self.nconv(x1,a)
+            for k in range(2, self.order + 1) :
+                x2 = self.nconv(x1, a)
                 out.append(x2)
                 x1 = x2
 
-        h = torch.cat(out,dim=1)
+        h = torch.cat(out, dim=1)
         h = self.mlp(h)
         h = F.dropout(h, self.dropout, training=self.training)
+
         return h
 
 
-class gwnet(nn.Module):
-    def __init__(self, device, num_nodes, dropout=0.3, supports=None, gcn_bool=True, addaptadj=True, aptinit=None, in_dim=2,out_dim=12,residual_channels=32,dilation_channels=32,skip_channels=256,end_channels=512,kernel_size=2,blocks=4,layers=2):
+class gwnet(nn.Module) :
+
+    def __init__(self, device, num_nodes, dropout=0.3, supports=None, gcn_bool=True, addaptadj=True, aptinit=None, in_dim=2, out_dim=12, residual_channels=32, dilation_channels=32, skip_channels=256, end_channels=512, kernel_size=2, blocks=4, layers=2) :
         super(gwnet, self).__init__()
         self.dropout = dropout
         self.blocks = blocks
@@ -62,26 +74,26 @@ class gwnet(nn.Module):
         self.bn = nn.ModuleList()
         self.gconv = nn.ModuleList()
 
-        self.start_conv = nn.Conv2d(in_channels=in_dim,
-                                    out_channels=residual_channels,
-                                    kernel_size=(1,1))
+        self.start_conv = nn.Conv2d(in_channels=in_dim, 
+                                    out_channels=residual_channels, 
+                                    kernel_size=(1, 1))
         self.supports = supports
 
         receptive_field = 1
 
         self.supports_len = 0
-        if supports is not None:
+        if supports is not None :
             self.supports_len += len(supports)
 
-        if gcn_bool and addaptadj:
-            if aptinit is None:
-                if supports is None:
+        if gcn_bool and addaptadj :
+            if aptinit is None :
+                if supports is None :
                     self.supports = []
                 self.nodevec1 = nn.Parameter(torch.randn(num_nodes, 10).to(device), requires_grad=True).to(device)
                 self.nodevec2 = nn.Parameter(torch.randn(10, num_nodes).to(device), requires_grad=True).to(device)
                 self.supports_len +=1
-            else:
-                if supports is None:
+            else :
+                if supports is None :
                     self.supports = []
                 m, p, n = torch.svd(aptinit)
                 initemb1 = torch.mm(m[:, :10], torch.diag(p[:10] ** 0.5))
@@ -92,69 +104,68 @@ class gwnet(nn.Module):
 
 
 
-
-        for b in range(blocks):
+        for b in range(blocks) :
             additional_scope = kernel_size - 1
             new_dilation = 1
-            for i in range(layers):
+            for i in range(layers) :
                 # dilated convolutions
-                self.filter_convs.append(nn.Conv2d(in_channels=residual_channels,
-                                                   out_channels=dilation_channels,
-                                                   kernel_size=(1,kernel_size),dilation=new_dilation))
+                self.filter_convs.append(nn.Conv2d(in_channels=residual_channels, 
+                                                   out_channels=dilation_channels, 
+                                                   kernel_size=(1, kernel_size), dilation=new_dilation))
 
-                self.gate_convs.append(nn.Conv2d(in_channels=residual_channels,
-                                                 out_channels=dilation_channels,
+                self.gate_convs.append(nn.Conv2d(in_channels=residual_channels, 
+                                                 out_channels=dilation_channels, 
                                                  kernel_size=(1, kernel_size), dilation=new_dilation))
 
                 # 1x1 convolution for residual connection
-                self.residual_convs.append(nn.Conv1d(in_channels=dilation_channels,
-                                                     out_channels=residual_channels,
+                self.residual_convs.append(nn.Conv1d(in_channels=dilation_channels, 
+                                                     out_channels=residual_channels, 
                                                      kernel_size=(1, 1)))
 
                 # 1x1 convolution for skip connection
-                self.skip_convs.append(nn.Conv2d(in_channels=dilation_channels,
-                                                 out_channels=skip_channels,
+                self.skip_convs.append(nn.Conv2d(in_channels=dilation_channels, 
+                                                 out_channels=skip_channels, 
                                                  kernel_size=(1, 1)))
                 self.bn.append(nn.BatchNorm2d(residual_channels))
-                new_dilation *=2
+                new_dilation *= 2
                 receptive_field += additional_scope
                 additional_scope *= 2
-                if self.gcn_bool:
-                    self.gconv.append(gcn(dilation_channels,residual_channels,dropout,support_len=self.supports_len))
+                if self.gcn_bool :
+                    self.gconv.append(gcn(dilation_channels, residual_channels, dropout, support_len=self.supports_len))
 
 
 
-        self.end_conv_1 = nn.Conv2d(in_channels=skip_channels,
-                                  out_channels=end_channels,
-                                  kernel_size=(1,1),
+        self.end_conv_1 = nn.Conv2d(in_channels=skip_channels, 
+                                  out_channels=end_channels, 
+                                  kernel_size=(1,1), 
                                   bias=True)
 
-        self.end_conv_2 = nn.Conv2d(in_channels=end_channels,
-                                    out_channels=out_dim,
-                                    kernel_size=(1,1),
+        self.end_conv_2 = nn.Conv2d(in_channels=end_channels, 
+                                    out_channels=out_dim, 
+                                    kernel_size=(1,1), 
                                     bias=True)
 
         self.receptive_field = receptive_field
 
 
 
-    def forward(self, input):
+    def forward(self, input) :
         in_len = input.size(3)
-        if in_len<self.receptive_field:
-            x = nn.functional.pad(input,(self.receptive_field-in_len,0,0,0))
-        else:
+        if in_len<self.receptive_field :
+            x = nn.functional.pad(input, (self.receptive_field-in_len, 0, 0, 0))
+        else :
             x = input
         x = self.start_conv(x)
         skip = 0
 
         # calculate the current adaptive adj matrix once per iteration
         new_supports = None
-        if self.gcn_bool and self.addaptadj and self.supports is not None:
+        if self.gcn_bool and self.addaptadj and self.supports is not None :
             adp = F.softmax(F.relu(torch.mm(self.nodevec1, self.nodevec2)), dim=1)
             new_supports = self.supports + [adp]
 
         # WaveNet layers
-        for i in range(self.blocks * self.layers):
+        for i in range(self.blocks * self.layers) :
 
             #            |----------------------------------------|     *residual*
             #            |                                        |
@@ -180,19 +191,19 @@ class gwnet(nn.Module):
 
             s = x
             s = self.skip_convs[i](s)
-            try:
-                skip = skip[:, :, :,  -s.size(3):]
-            except:
+            try :
+                skip = skip[:, :, :, -s.size(3):]
+            except :
                 skip = 0
             skip = s + skip
 
 
-            if self.gcn_bool and self.supports is not None:
-                if self.addaptadj:
+            if self.gcn_bool and self.supports is not None :
+                if self.addaptadj :
                     x = self.gconv[i](x, new_supports)
-                else:
+                else :
                     x = self.gconv[i](x,self.supports)
-            else:
+            else :
                 x = self.residual_convs[i](x)
 
             x = x + residual[:, :, :, -x.size(3):]
@@ -203,9 +214,7 @@ class gwnet(nn.Module):
         x = F.relu(skip)
         x = F.relu(self.end_conv_1(x))
         x = self.end_conv_2(x)
+
         return x
-
-
-
-
-
+    
+    
